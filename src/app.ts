c37004d45baaa84a13327d0bca59f645f408a3af
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-12-27 15:35:32 +0900

    エラー画面改善

--- src/app.ts
+++ src/app.ts
@@ -56,6 +56,7 @@ app.use(function(req, res, next) {
 app.use(function(err: any, req:express.Request, res:express.Response, next:express.NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  res.locals.status = err.status;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
