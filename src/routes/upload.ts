9136cf239044bb73d13cc1ef7d4288fb9c99be2d
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-12-28 22:22:54 +0900

    リファクタリング

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -15,8 +15,8 @@ router.get("/", ensureAuthenticated, function (req, res) {
 
 //WebGLのアップロード
 function getWebglDir(req: express.Request): string {
-  const creator_id = req.headers['x-creator-id'] as string;
-  const game_id = req.headers['x-game-id'] as string;
+  const creator_id = req.headers["x-creator-id"] as string;
+  const game_id = req.headers["x-game-id"] as string;
   return path.join(GAMES_DIRECTORY_NAME, creator_id, game_id, "webgl");
 }
 // function validateWebglUpload(
@@ -94,24 +94,33 @@ const webglUpload = multer({
 router.post(
   "/webgl",
   ensureAuthenticated,
-  (req, res, next)=>{
-    const creatorId = req.headers['x-creator-id'];
-    const gameId = req.headers['x-game-id'];
-    if(typeof creatorId !== 'string' || typeof gameId !== 'string' || creatorId.length == 0 || gameId.length == 0){
-      res.status(500).send('作者IDまたはゲームIDが指定されていません。');
+  (req, res, next) => {
+    const creatorId = req.headers["x-creator-id"];
+    const gameId = req.headers["x-game-id"];
+    if (
+      typeof creatorId !== "string" ||
+      typeof gameId !== "string" ||
+      creatorId.length == 0 ||
+      gameId.length == 0
+    ) {
+      res.status(500).send("作者IDまたはゲームIDが指定されていません。");
       return;
     }
     const gameDir = path.join("uploads", getWebglDir(req));
     if (fs.existsSync(gameDir)) {
-      const overwritesExisting = req.headers['x-overwrites-existing'] as string;
+      const overwritesExisting = req.headers["x-overwrites-existing"] as string;
       console.log(overwritesExisting);
       if (overwritesExisting !== "true") {
-        res.status(500).send("ゲームが既に存在しています。上書きする場合はチェックボックスにチェックを入れてください");
+        res
+          .status(500)
+          .send(
+            "ゲームが既に存在しています。上書きする場合はチェックボックスにチェックを入れてください"
+          );
         return;
       }
     }
     next();
-  }, 
+  },
   webglUpload.array("game"),
   function (req, res) {
     const files = req.files;
