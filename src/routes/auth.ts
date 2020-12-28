be2e73977f2f4cb98713aa365bd5206a94179240
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-12-28 12:43:23 +0900

    services/auth.tsのリファクタリング

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,27 +3,42 @@ import request from "request";
 import jwt from "jsonwebtoken";
 import { isAuthenticated, redirect } from "../services/auth";
 const router = express.Router();
+interface SlackAuthResponse {
+  ok: boolean;
+  app_id: string;
+  authed_user : {
+    id: string;
+    scope: string;
+    access_token: string;
+    token_type: string;
+  };
+  team: {
+    id: string;
+  };
+  enterprise: unknown;
+  is_enterprise_install: boolean;
+}
 
 /* GET home page. */
-router.get("/", function (req, res, _) {
+router.get("/", function (req, res) {
   if (isAuthenticated(req)) {
     res.redirect("/");
     return;
   }
   res.render("login");
 });
-router.get("/redirect", (req, res, _) => {
+router.get("/redirect", (req, res) => {
   const code = req.query.code;
-  if (!code) {
+  if (typeof(code) !== 'string') {
     res.redirect("/auth");
     return;
   }
   const options = {
-    uri: `https://slack.com/api/oauth.v2.access?code=${code}&client_id=${process.env.SLACK_CLIENT_ID}&client_secret=${process.env.SLACK_CLIENT_SECRET}`,
+    uri: `https://slack.com/api/oauth.v2.access?code=${code}&client_id=${process.env.SLACK_CLIENT_ID ?? ""}&client_secret=${process.env.SLACK_CLIENT_SECRET ?? ""}`,
     method: "GET",
   };
   request(options, (error, response, body) => {
-    const JSONresponse = JSON.parse(body);
+    const JSONresponse = JSON.parse(body) as SlackAuthResponse;
     if (!JSONresponse.ok) {
       res
         .send("Error encountered: \n" + JSON.stringify(JSONresponse))
@@ -40,7 +55,7 @@ router.get("/redirect", (req, res, _) => {
   });
 });
 
-router.all("/logout", (req, res, _) => {
+router.all("/logout", (req, res) => {
   req.session.token = undefined;
   req.session.oauth = undefined;
   res.redirect("/auth");
