b0d5483f162ddd3b00900ba9b3a28d5e03d85e54
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-12-31 23:47:03 +0900

    log4jsのcategory修正

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -6,12 +6,13 @@ log4js.configure({
     accessFile: {type: 'file', filename: 'access.log'}
   },
   categories: {
-    default: { appenders: ['console', 'file'], level: 'all' },
+    default: {appenders: ['console'],level: 'info'},
+    system: { appenders: ['console', 'file'], level: 'info' },
     access: { appenders: ['console','accessFile'], level: 'info' }
   }
 });
 const access = log4js.getLogger('access');
-const system = log4js.getLogger('default');
+const system = log4js.getLogger('system');
 const connectLogger = log4js.connectLogger;
 
 export { 
