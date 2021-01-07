a984b3ebd44e77f4b199fd60e9ec5621f38905a7
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-01-07 12:36:43 +0900

    リファクタリング

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -46,58 +46,59 @@ const webglUpload = multer({
     cb(null, !folders.find((f) => f.startsWith(".")));
   },
 });
-
-router.post(
-  "/webgl",
-  ensureAuthenticated,
-  (req, res, next) => {
-    const creatorId = req.headers["x-creator-id"];
-    const gameId = req.headers["x-game-id"];
-    if (
-      typeof creatorId !== "string" ||
-      typeof gameId !== "string" ||
-      creatorId.length == 0 ||
-      gameId.length == 0
-    ) {
-      logger.system.error(
-        "アップロード失敗:作者IDとゲームIDを両方を指定する必要があります。",
-        creatorId,
-        gameId
+function validateParams(req: express.Request, res: express.Response, next: express.NextFunction){
+  const creatorId = req.headers["x-creator-id"];
+  const gameId = req.headers["x-game-id"];
+  if (
+    typeof creatorId !== "string" ||
+    typeof gameId !== "string" ||
+    creatorId.length == 0 ||
+    gameId.length == 0
+  ) {
+    logger.system.error(
+      "アップロード失敗:作者IDとゲームIDを両方を指定する必要があります。",
+      creatorId,
+      gameId
+    );
+    res.status(500).send("作者IDとゲームIDを両方を指定する必要があります。");
+    return;
+  }
+  if (!/^[0-9a-z-]+$/.test(creatorId) || !/^[0-9a-z-]+$/.test(gameId)) {
+    logger.system.error(
+      "アップロード失敗:作者IDおよびゲームIDは数字・アルファベット小文字・ハイフンのみ使用できます。",
+      creatorId,
+      gameId
+    );
+    res
+      .status(500)
+      .send(
+        "作者IDおよびゲームIDは数字・アルファベット小文字・ハイフンのみ使用できます。"
       );
-      res.status(500).send("作者IDとゲームIDを両方を指定する必要があります。");
-      return;
-    }
-    if (!/^[0-9a-z-]+$/.test(creatorId) || !/^[0-9a-z-]+$/.test(gameId)) {
+    return;
+  }
+  const gameDir = path.join(DIRECTORY_UPLOADS_DESTINATION, getWebglDir(req));
+  if (fs.existsSync(gameDir)) {
+    const overwritesExisting = req.headers["x-overwrites-existing"] as string;
+    if (overwritesExisting !== "true") {
       logger.system.error(
-        "アップロード失敗:作者IDおよびゲームIDは数字・アルファベット小文字・ハイフンのみ使用できます。",
-        creatorId,
-        gameId
+        "アップロード失敗:ゲームが既に存在しています。",
+        gameDir
       );
       res
         .status(500)
         .send(
-          "作者IDおよびゲームIDは数字・アルファベット小文字・ハイフンのみ使用できます。"
+          "ゲームが既に存在しています。上書きする場合はチェックボックスにチェックを入れてください"
         );
       return;
     }
-    const gameDir = path.join(DIRECTORY_UPLOADS_DESTINATION, getWebglDir(req));
-    if (fs.existsSync(gameDir)) {
-      const overwritesExisting = req.headers["x-overwrites-existing"] as string;
-      if (overwritesExisting !== "true") {
-        logger.system.error(
-          "アップロード失敗:ゲームが既に存在しています。",
-          gameDir
-        );
-        res
-          .status(500)
-          .send(
-            "ゲームが既に存在しています。上書きする場合はチェックボックスにチェックを入れてください"
-          );
-        return;
-      }
-    }
-    next();
-  },
+  }
+  next();
+}
+
+router.post(
+  "/webgl",
+  ensureAuthenticated,
+  validateParams,
   webglUpload.array("game"),
   function (req, res) {
     const files = req.files ?? [];
