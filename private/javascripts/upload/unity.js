140f37fe686cb2759e7f89742325dabf081fa048
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-01-20 13:37:39 +0900

    ブラウザー用コードをTypescriptに移行

--- private/javascripts/upload/unity.js
+++ private/javascripts/upload/unity.js
@@ -1,163 +1,171 @@
-//querySelector
-const webglFilesInput = document.querySelector('input[name="webgl"]');
-const windowsFilesInput = document.querySelector('input[name="windows"]');
-const webglFileFieldFrame = document.querySelector(".input-field.file-field-frame.webgl");
-const windowsFileFieldFrame = document.querySelector(".input-field.file-field-frame.windows");
-const fileList = document.querySelector('.file-list');
-const creatorIdInput = document.querySelector('input[name="creator_id"]');
-const gameIdInput = document.querySelector('input[name="game_id"]');
-const form = document.querySelector('form');
-const uploadButton = document.querySelector('.uploadButton');
-const uploadingIndicator = document.querySelector('.uploadingIndicator');
-
-//Materializeのロード
-document.addEventListener('DOMContentLoaded', function () {
-  M.Collapsible.init(document.querySelector('.collapsible'), {
-    onOpenStart: (elm) => {
-      elm.querySelector('.collapsible-header>.material-icons').innerHTML = 'keyboard_arrow_up';
-    },
-    onCloseStart: (elm) => {
-      elm.querySelector('.collapsible-header>.material-icons').innerHTML = 'keyboard_arrow_down';
-    },
-  });
-  M.Modal.init(document.querySelector("#result-dialog"), {});
+"use strict";
+var webglFilesInput = document.querySelector('input[name="webgl"]');
+var windowsFilesInput = document.querySelector('input[name="windows"]');
+var webglFileFieldFrame = document.querySelector(".input-field.file-field-frame.webgl");
+var windowsFileFieldFrame = document.querySelector(".input-field.file-field-frame.windows");
+var fileList = document.querySelector(".file-list");
+var creatorIdInput = document.querySelector('input[name="creator_id"]');
+var gameIdInput = document.querySelector('input[name="game_id"]');
+var form = document.querySelector("form");
+var uploadButton = document.querySelector(".uploadButton");
+var uploadingIndicator = document.querySelector(".uploadingIndicator");
+document.addEventListener("DOMContentLoaded", function () {
+    M.Collapsible.init(document.querySelector(".collapsible"), {
+        onOpenStart: function (elm) {
+            var header = elm.querySelector(".collapsible-header>.material-icons");
+            header.innerHTML = "keyboard_arrow_up";
+        },
+        onCloseStart: function (elm) {
+            var header = elm.querySelector(".collapsible-header>.material-icons");
+            header.innerHTML = "keyboard_arrow_down";
+        },
+    });
+    M.Modal.init(document.querySelector("#result-dialog"), {});
 });
-
-//ドラッグ&ドロップ時のUI変化
 function initDragAndDrop(fileInput, frame) {
-  fileInput.addEventListener("dragenter", (event) => {
-    frame.classList.add("drag");
-  });
-  fileInput.addEventListener("dragleave", (event) => {
-    frame.classList.remove("drag");
-  });
-  fileInput.addEventListener("drop", (event) => {
-    frame.classList.remove("drag");
-  });
-
+    fileInput.addEventListener("dragenter", function (event) {
+        frame.classList.add("drag");
+    });
+    fileInput.addEventListener("dragleave", function (event) {
+        frame.classList.remove("drag");
+    });
+    fileInput.addEventListener("drop", function (event) {
+        frame.classList.remove("drag");
+    });
 }
 initDragAndDrop(webglFilesInput, webglFileFieldFrame);
 initDragAndDrop(windowsFilesInput, windowsFileFieldFrame);
-
-//ドロップ時の動作
-webglFilesInput.addEventListener('change', (event) => onFilesDropped('webgl', webglFilesInput));
-windowsFilesInput.addEventListener('change', (event) => onFilesDropped('windows', windowsFilesInput));
-
+webglFilesInput.addEventListener("change", function (event) {
+    return onFilesDropped("webgl", webglFilesInput);
+});
+windowsFilesInput.addEventListener("change", function (event) {
+    return onFilesDropped("windows", windowsFilesInput);
+});
 function onMultipleFilesDropped(type, input) {
-  const message = document.querySelector(`.message-hidden-folder-files.${type}`)
-  message.classList.add('hide');
-  const dt = new DataTransfer();
-  Array.from(input.files).filter((file) => {//隠しフォルダ内のファイルを除去
-    const directories = file.webkitRelativePath.split('/');
-    return directories.find((dir) => dir.startsWith('.')) === undefined;
-  }).forEach((file) => {
-    dt.items.add(file);
-  });
-  const fileCountBefore = input.files.length;
-  const fileCountAfter = dt.files.length;
-  if (fileCountBefore > fileCountAfter) {
-    message.classList.remove('hide');
-  }
-  input.files = dt.files;
-}
-
-function onFilesDropped(type, input) {
-  const preview = fileList.querySelector(`.${type}`);
-  preview.innerHTML = `${type}`;
-  const filePaths = [];
-  if (input.webkitdirectory) {
-    onMultipleFilesDropped(type, input);
-    Array.from(input.files).forEach((file) => {
-      filePaths.push(file.webkitRelativePath.replace(/^[^\/]+\//, ''));
+    var _a, _b, _c;
+    var message = document.querySelector(".message-hidden-folder-files." + type);
+    message.classList.add("hide");
+    var dt = new DataTransfer();
+    Array.from((_a = input.files) !== null && _a !== void 0 ? _a : new FileList())
+        .filter(function (file) {
+        var directories = file.webkitRelativePath.split("/");
+        return directories.find(function (dir) { return dir.startsWith("."); }) === undefined;
+    })
+        .forEach(function (file) {
+        dt.items.add(file);
     });
-  } else {
-    filePaths.push(input.files[0].name);
-  }
-  filePaths.sort();
-  filePaths.forEach((path) => {
-    let parent = preview;
-    path.split('/').forEach((section) => {
-      let p_ul = Array.from(parent.childNodes).find((node) => node.tagName === 'UL');
-      if (!p_ul) {
-        p_ul = document.createElement('ul');
-        parent.appendChild(p_ul);
-      }
-      let li = Array.from(p_ul.childNodes).find((node) => node.tagName === 'LI' && node.getAttribute('name') == section);
-      if (!li) {
-        li = document.createElement('li');
-        li.innerHTML = section;
-        li.setAttribute('name', section);
-        p_ul.appendChild(li);
-      }
-      parent = li;
-    });
-  });
-}
-
-
-//アップロード先URL表示の同期
-creatorIdInput.addEventListener('change', (ev) => {
-  let creatorId = creatorIdInput.value;
-  if (creatorId.length == 0) {
-    creatorId = '(作者ID)';
-  }
-  document.querySelector('.file-list-header>.creator_id').innerHTML = creatorId;
-});
-gameIdInput.addEventListener('change', (ev) => {
-  let gameId = gameIdInput.value;
-  if (gameId.length == 0) {
-    gameId = '(ゲームID)';
-  }
-  document.querySelector('.file-list-header>.game_id').innerHTML = gameId;
-});
-
-//ファイルアップロード
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  if (webglFilesInput.files.length == 0 && windowsFilesInput.files.length == 0) {
-    return;
-  }
-  setUploading(true);
-  const data = new FormData(form);
-  const request = new XMLHttpRequest();
-  request.open('POST', '', true);
-  request.setRequestHeader('x-creator-id', document.querySelector('input[name="creator_id"]').value)
-  request.setRequestHeader('x-game-id', document.querySelector('input[name="game_id"]').value)
-  request.setRequestHeader('x-overwrites-existing', document.querySelector('input[name="overwrites_existing"]').checked)
-  request.addEventListener('load', (ev) => {
-    setUploading(false);
-    let content = `<h4>${(request.status === 200) ? "アップロードに成功しました" : "アップロードに失敗しました"}</h4>`;
-    if (request.status === 200) {
-      const paths = JSON.parse(request.response).paths?.map((p)=>p.replaceAll("\\", "/")) ?? [];
-      if (paths.length > 0) {
-        paths.forEach((p)=>{
-          const url = `${location.origin}${p}`;
-          content += `<p><a href="${url}">${url}</a>にアップロードしました。</p>`;  
-        })
-      }
-    } else if (request.status === 401){
-      content += "<p>ログインしなおす必要があります。</p><p>ページを再読み込みしてください</>";
-    }else{
-      content += request.response;
+    var fileCountBefore = (_c = (_b = input.files) === null || _b === void 0 ? void 0 : _b.length) !== null && _c !== void 0 ? _c : [];
+    var fileCountAfter = dt.files.length;
+    if (fileCountBefore > fileCountAfter) {
+        message.classList.remove("hide");
     }
-    const dialog = document.querySelector("#result-dialog");
-    dialog.querySelector('.modal-content').innerHTML = content;
-    M.Modal.getInstance(dialog).open();
-  })
-  request.send(data);
+    input.files = dt.files;
+}
+function onFilesDropped(type, input) {
+    var _a, _b, _c;
+    var preview = fileList.querySelector("." + type);
+    preview.innerHTML = "" + type;
+    var filePaths = [];
+    if (input.webkitdirectory) {
+        onMultipleFilesDropped(type, input);
+        Array.from((_a = input.files) !== null && _a !== void 0 ? _a : new FileList()).forEach(function (file) {
+            filePaths.push(file.webkitRelativePath.replace(/^[^\/]+\//, ""));
+        });
+    }
+    else {
+        filePaths.push((_c = ((_b = input.files) !== null && _b !== void 0 ? _b : [])[0]) === null || _c === void 0 ? void 0 : _c.name);
+    }
+    filePaths.sort();
+    filePaths.forEach(function (path) {
+        var parent = preview;
+        path.split("/").forEach(function (section) {
+            var p_ul = Array.from(parent.childNodes).find(function (node) { return node.tagName === "UL"; });
+            if (!p_ul) {
+                p_ul = document.createElement("ul");
+                parent.appendChild(p_ul);
+            }
+            var li = Array.from(p_ul.childNodes).find(function (node) { return node.tagName === "LI" && node.getAttribute("name") == section; });
+            if (!li) {
+                li = document.createElement("li");
+                li.innerHTML = section;
+                li.setAttribute("name", section);
+                p_ul.appendChild(li);
+            }
+            parent = li;
+        });
+    });
+}
+creatorIdInput.addEventListener("change", function (ev) {
+    var creatorId = creatorIdInput.value;
+    if (creatorId.length == 0) {
+        creatorId = "(作者ID)";
+    }
+    document.querySelector(".file-list-header>.creator_id").innerHTML = creatorId;
+});
+gameIdInput.addEventListener("change", function (ev) {
+    var gameId = gameIdInput.value;
+    if (gameId.length == 0) {
+        gameId = "(ゲームID)";
+    }
+    document.querySelector(".file-list-header>.game_id").innerHTML = gameId;
+});
+form.addEventListener("submit", function (event) {
+    var _a, _b;
+    event.preventDefault();
+    if (((_a = webglFilesInput.files) === null || _a === void 0 ? void 0 : _a.length) === 0 &&
+        ((_b = windowsFilesInput.files) === null || _b === void 0 ? void 0 : _b.length) === 0) {
+        return;
+    }
+    setUploading(true);
+    var data = new FormData(form);
+    var request = new XMLHttpRequest();
+    request.open("POST", "", true);
+    request.setRequestHeader("x-creator-id", creatorIdInput.value);
+    request.setRequestHeader("x-game-id", gameIdInput.value);
+    request.setRequestHeader("x-overwrites-existing", document.querySelector('input[name="overwrites_existing"]').checked.toString());
+    request.addEventListener("load", function (ev) {
+        var _a, _b;
+        setUploading(false);
+        var content = "<h4>" + (request.status === 200
+            ? "アップロードに成功しました"
+            : "アップロードに失敗しました") + "</h4>";
+        if (request.status === 200) {
+            var paths = (_b = (_a = JSON.parse(request.response).paths) === null || _a === void 0 ? void 0 : _a.map(function (p) {
+                return p.replace(/\\/g, "/");
+            })) !== null && _b !== void 0 ? _b : [];
+            if (paths.length > 0) {
+                paths.forEach(function (p) {
+                    var url = "" + location.origin + p;
+                    content += "<p><a href=\"" + url + "\">" + url + "</a>\u306B\u30A2\u30C3\u30D7\u30ED\u30FC\u30C9\u3057\u307E\u3057\u305F\u3002</p>";
+                });
+            }
+        }
+        else if (request.status === 401) {
+            content +=
+                "<p>ログインしなおす必要があります。</p><p>ページを再読み込みしてください</>";
+        }
+        else {
+            content += request.response;
+        }
+        var dialog = document.querySelector("#result-dialog");
+        dialog.querySelector(".modal-content").innerHTML = content;
+        M.Modal.getInstance(dialog).open();
+    });
+    request.send(data);
 });
 function alertBeforeLeave(event) {
-  event.preventDefault();
-  event.returnValue = '';
+    event.preventDefault();
+    event.returnValue = "";
 }
 function setUploading(uploading) {
-  if (uploading) {
-    uploadButton.classList.add('disabled');
-    uploadingIndicator.classList.remove('hide');
-    window.addEventListener('beforeunload', alertBeforeLeave);
-  } else {
-    uploadButton.classList.remove('disabled');
-    uploadingIndicator.classList.add('hide');
-    window.removeEventListener('beforeunload', alertBeforeLeave);
-  }
+    if (uploading) {
+        uploadButton.classList.add("disabled");
+        uploadingIndicator.classList.remove("hide");
+        window.addEventListener("beforeunload", alertBeforeLeave);
+    }
+    else {
+        uploadButton.classList.remove("disabled");
+        uploadingIndicator.classList.add("hide");
+        window.removeEventListener("beforeunload", alertBeforeLeave);
+    }
 }
---
+++ src_browser/private/javascripts/upload/unity.ts
@@ -0,0 +1,218 @@
+//querySelector
+const webglFilesInput = document.querySelector(
+  'input[name="webgl"]'
+) as HTMLInputElement;
+const windowsFilesInput = document.querySelector(
+  'input[name="windows"]'
+) as HTMLInputElement;
+const webglFileFieldFrame = document.querySelector(
+  ".input-field.file-field-frame.webgl"
+) as HTMLDivElement;
+const windowsFileFieldFrame = document.querySelector(
+  ".input-field.file-field-frame.windows"
+) as HTMLDivElement;
+const fileList = document.querySelector(".file-list") as HTMLDivElement;
+const creatorIdInput = document.querySelector(
+  'input[name="creator_id"]'
+) as HTMLInputElement;
+const gameIdInput = document.querySelector(
+  'input[name="game_id"]'
+) as HTMLInputElement;
+const form = document.querySelector("form") as HTMLFormElement;
+const uploadButton = document.querySelector(
+  ".uploadButton"
+) as HTMLButtonElement;
+const uploadingIndicator = document.querySelector(
+  ".uploadingIndicator"
+) as HTMLElement;
+
+//Materializeのロード
+document.addEventListener("DOMContentLoaded", function () {
+  M.Collapsible.init(document.querySelector(".collapsible") as Element, {
+    onOpenStart: (elm) => {
+      const header = elm.querySelector(
+        ".collapsible-header>.material-icons"
+      ) as HTMLElement;
+      header.innerHTML = "keyboard_arrow_up";
+    },
+    onCloseStart: (elm) => {
+      const header = elm.querySelector(
+        ".collapsible-header>.material-icons"
+      ) as HTMLElement;
+      header.innerHTML = "keyboard_arrow_down";
+    },
+  });
+  M.Modal.init(document.querySelector("#result-dialog") as HTMLElement, {});
+});
+
+//ドラッグ&ドロップ時のUI変化
+function initDragAndDrop(fileInput: HTMLInputElement, frame: HTMLDivElement) {
+  fileInput.addEventListener("dragenter", (event) => {
+    frame.classList.add("drag");
+  });
+  fileInput.addEventListener("dragleave", (event) => {
+    frame.classList.remove("drag");
+  });
+  fileInput.addEventListener("drop", (event) => {
+    frame.classList.remove("drag");
+  });
+}
+initDragAndDrop(webglFilesInput, webglFileFieldFrame);
+initDragAndDrop(windowsFilesInput, windowsFileFieldFrame);
+
+//ドロップ時の動作
+webglFilesInput.addEventListener("change", (event) =>
+  onFilesDropped("webgl", webglFilesInput)
+);
+windowsFilesInput.addEventListener("change", (event) =>
+  onFilesDropped("windows", windowsFilesInput)
+);
+
+function onMultipleFilesDropped(type: string, input: HTMLInputElement) {
+  const message = document.querySelector(
+    `.message-hidden-folder-files.${type}`
+  ) as HTMLDivElement;
+  message.classList.add("hide");
+  const dt = new DataTransfer();
+  Array.from(input.files ?? new FileList())
+    .filter((file) => {
+      //隠しフォルダ内のファイルを除去
+      const directories = (file as any).webkitRelativePath.split(
+        "/"
+      ) as string[];
+      return directories.find((dir) => dir.startsWith(".")) === undefined;
+    })
+    .forEach((file) => {
+      dt.items.add(file);
+    });
+  const fileCountBefore = input.files?.length ?? [];
+  const fileCountAfter = dt.files.length;
+  if (fileCountBefore > fileCountAfter) {
+    message.classList.remove("hide");
+  }
+  input.files = dt.files;
+}
+
+function onFilesDropped(type: string, input: HTMLInputElement) {
+  const preview = fileList.querySelector(`.${type}`) as HTMLElement;
+  preview.innerHTML = `${type}`;
+  const filePaths = [];
+  if ((input as any).webkitdirectory) {
+    onMultipleFilesDropped(type, input);
+    Array.from(input.files ?? new FileList()).forEach((file) => {
+      filePaths.push((file as any).webkitRelativePath.replace(/^[^\/]+\//, ""));
+    });
+  } else {
+    filePaths.push((input.files ?? [])[0]?.name);
+  }
+  filePaths.sort();
+  filePaths.forEach((path) => {
+    let parent = preview;
+    path.split("/").forEach((section) => {
+      let p_ul = (Array.from(parent.childNodes) as Element[]).find(
+        (node) => node.tagName === "UL"
+      );
+      if (!p_ul) {
+        p_ul = document.createElement("ul");
+        parent.appendChild(p_ul);
+      }
+      let li = (Array.from(p_ul.childNodes) as Element[]).find(
+        (node) => node.tagName === "LI" && node.getAttribute("name") == section
+      ) as HTMLElement;
+      if (!li) {
+        li = document.createElement("li");
+        li.innerHTML = section;
+        li.setAttribute("name", section);
+        p_ul.appendChild(li);
+      }
+      parent = li;
+    });
+  });
+}
+
+//アップロード先URL表示の同期
+creatorIdInput.addEventListener("change", (ev) => {
+  let creatorId = creatorIdInput.value;
+  if (creatorId.length == 0) {
+    creatorId = "(作者ID)";
+  }
+  (document.querySelector(
+    ".file-list-header>.creator_id"
+  ) as HTMLDivElement).innerHTML = creatorId;
+});
+gameIdInput.addEventListener("change", (ev) => {
+  let gameId = gameIdInput.value;
+  if (gameId.length == 0) {
+    gameId = "(ゲームID)";
+  }
+  (document.querySelector(
+    ".file-list-header>.game_id"
+  ) as HTMLDivElement).innerHTML = gameId;
+});
+
+//ファイルアップロード
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (
+    webglFilesInput.files?.length === 0 &&
+    windowsFilesInput.files?.length === 0
+  ) {
+    return;
+  }
+  setUploading(true);
+  const data = new FormData(form);
+  const request = new XMLHttpRequest();
+  request.open("POST", "", true);
+  request.setRequestHeader("x-creator-id", creatorIdInput.value);
+  request.setRequestHeader("x-game-id", gameIdInput.value);
+  request.setRequestHeader(
+    "x-overwrites-existing",
+    (document.querySelector(
+      'input[name="overwrites_existing"]'
+    ) as HTMLInputElement).checked.toString()
+  );
+  request.addEventListener("load", (ev) => {
+    setUploading(false);
+    let content = `<h4>${
+      request.status === 200
+        ? "アップロードに成功しました"
+        : "アップロードに失敗しました"
+    }</h4>`;
+    if (request.status === 200) {
+      const paths: string[] =
+        JSON.parse(request.response).paths?.map((p: string) =>
+          p.replace(/\\/g, "/")
+        ) ?? [];
+      if (paths.length > 0) {
+        paths.forEach((p) => {
+          const url = `${location.origin}${p}`;
+          content += `<p><a href="${url}">${url}</a>にアップロードしました。</p>`;
+        });
+      }
+    } else if (request.status === 401) {
+      content +=
+        "<p>ログインしなおす必要があります。</p><p>ページを再読み込みしてください</>";
+    } else {
+      content += request.response;
+    }
+    const dialog = document.querySelector("#result-dialog") as HTMLElement;
+    (dialog.querySelector(".modal-content") as HTMLElement).innerHTML = content;
+    M.Modal.getInstance(dialog).open();
+  });
+  request.send(data);
+});
+function alertBeforeLeave(event: BeforeUnloadEvent) {
+  event.preventDefault();
+  event.returnValue = "";
+}
+function setUploading(uploading: boolean) {
+  if (uploading) {
+    uploadButton.classList.add("disabled");
+    uploadingIndicator.classList.remove("hide");
+    window.addEventListener("beforeunload", alertBeforeLeave);
+  } else {
+    uploadButton.classList.remove("disabled");
+    uploadingIndicator.classList.add("hide");
+    window.removeEventListener("beforeunload", alertBeforeLeave);
+  }
+}
