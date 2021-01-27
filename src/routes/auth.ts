6200c9b7c2705361f7c8215733637b6c7c5d83ac
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-01-27 14:16:41 +0900

    フォーマット

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import * as logger from "../modules/logger";
 import { passport } from "../app";
 import { getContentSecurityPolicy, getEnv } from "../helpers";
+import * as logger from "../modules/logger";
 import { isAuthenticated, redirect } from "../services/auth";
 
 const authRouter = express.Router();
@@ -32,18 +32,18 @@ authRouter.get(
   passport.authenticate("slack", {
     failureRedirect: "/auth/error",
   }),
-  (req,res,next)=>{
-    const user = req.user as {team: {id: string}, user: {id: string}};
+  (req, res, next) => {
+    const user = req.user as { team: { id: string }; user: { id: string } };
     const workspaceId = user?.team?.id;
-    if(workspaceId !== getEnv("SLACK_WORKSPACE_ID")){
-      logger.system.error(	
-        `違うワークスペース${workspaceId}の人がログインしようとしました。`	
-      );	
-      res.send("認証に失敗しました。").status(403).end();	
-      return;	
+    if (workspaceId !== getEnv("SLACK_WORKSPACE_ID")) {
+      logger.system.error(
+        `違うワークスペース${workspaceId}の人がログインしようとしました。`
+      );
+      res.send("認証に失敗しました。").status(403).end();
+      return;
     }
-    logger.system.info(	
-      `ユーザー${user.user.id}がSlack認証でログインしました。`	
+    logger.system.info(
+      `ユーザー${user.user.id}がSlack認証でログインしました。`
     );
     next();
   },
