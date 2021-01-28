ea548708d1428f2cdb08c0e98b6e9da70ae245ab
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-01-28 19:24:45 +0900

    serve-indexでejsを使うテスト
    TODO：ejsに移行

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,14 +1,40 @@
 import fs from "fs";
 import path from "path";
+// import ejs from "ejs";
 import escapeHtml from "escape-html";
 import express from "express";
 import serveIndex from "serve-index";
 import { DIRECTORY_UPLOADS_DESTINATION } from "../services/upload";
 const gamesRouter = express.Router();
+// const viewsDir = path.join(__dirname, "../../views");
 gamesRouter.use(
   express.static(path.join(__dirname, "../..", DIRECTORY_UPLOADS_DESTINATION)),
   serveIndex(DIRECTORY_UPLOADS_DESTINATION, {
     template: renderTemplate,
+    //TODO：ejsに移行
+    // template: (
+    //   locals: serveIndex.Locals,
+    //   callback: serveIndex.templateCallback
+    // ) => {
+    //   ejs
+    //     .renderFile(
+    //       path.join(viewsDir, "games.ejs"),
+    //       {
+    //         ...locals,
+    //       },
+    //       {
+    //         beautify: true
+    //       }
+    //     )
+    //     .then((str) =>
+    //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //       // @ts-ignore
+    //       callback(null, str)
+    //     )
+    //     .catch((e) => {
+    //       callback(e);
+    //     });
+    // },
   })
 );
 function directoryBreadcrumbs(directory: string): string {
