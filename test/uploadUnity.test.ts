fc1d9aacc96ec7e4bf1d6419525fd8f61b3bc1bf
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-02-28 17:40:43 +0900

    単体テストを一通り実装

--- test/uploadUnity.test.ts
+++ test/uploadUnity.test.ts
@@ -3,16 +3,24 @@ import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../src/app";
 import { FIELD_WINDOWS } from "../src/services/upload";
-import { DIRECTORY_UPLOADS_DESTINATION, STATUS_CODE_UNAUTHORIZED } from "../src/utils/constants";
+import {
+  URL_PREFIX_GAME,
+  DIRECTORY_UPLOADS_DESTINATION,
+  STATUS_CODE_FAILURE,
+  STATUS_CODE_SUCCESS,
+  STATUS_CODE_UNAUTHORIZED,
+} from "../src/utils/constants";
 import { getEnv, getEnvNumber } from "../src/utils/helpers";
-import { login, logout, myId } from "./auth";
+import { login, logout, myId, theirId } from "./auth";
 import { clearData, connectDatabase } from "./common";
 import fs from "fs-extra";
 import path from "path";
+import { GameModel } from "../src/models/database";
 
 const creatorId = "creator";
 const gameId = "game";
-const unityGameWindowsPath = path.join(__dirname, "unity-correct-zip.zip");
+const unityGameWindowsName = "unity-correct-zip.zip";
+const unityGameWindowsPath = path.join(__dirname, unityGameWindowsName);
 
 describe("Unityゲームのアップロード", () => {
   before(async () => {
@@ -27,6 +35,121 @@ describe("Unityゲームのアップロード", () => {
   describe("ログイン時", () => {
     before(() => login(app, myId));
     after(() => logout(app));
+
+    it("作者IDが設定されていないとアップロードできない", (done) => {
+      request(app)
+        .post("/upload/unity")
+        .set("x-game-id", gameId)
+        .set("x-overwrites-existing", "false")
+        .attach(FIELD_WINDOWS, unityGameWindowsPath)
+        .expect(STATUS_CODE_FAILURE)
+        .expect("作者IDは必須です。")
+        .end(done);
+    });
+    it("作者IDが設定されていないとアップロードできない", (done) => {
+      request(app)
+        .post("/upload/unity")
+        .set("x-creator-id", encodeURI("テスト"))
+        .set("x-game-id", gameId)
+        .set("x-overwrites-existing", "false")
+        .attach(FIELD_WINDOWS, unityGameWindowsPath)
+        .expect(STATUS_CODE_FAILURE)
+        .expect(
+          "作者IDには数字・アルファベット小文字・ハイフンのみ使用できます。"
+        )
+        .end(done);
+    });
+    it("ゲームIDが設定されていないとアップロードできない", (done) => {
+      request(app)
+        .post("/upload/unity")
+        .set("x-creator-id", creatorId)
+        .set("x-overwrites-existing", "false")
+        .attach(FIELD_WINDOWS, unityGameWindowsPath)
+        .expect(STATUS_CODE_FAILURE)
+        .expect("ゲームIDは必須です。")
+        .end(done);
+    });
+    it("作者IDが設定されていないとアップロードできない", (done) => {
+      request(app)
+        .post("/upload/unity")
+        .set("x-creator-id", creatorId)
+        .set("x-game-id", encodeURI("テスト"))
+        .set("x-overwrites-existing", "false")
+        .attach(FIELD_WINDOWS, unityGameWindowsPath)
+        .expect(STATUS_CODE_FAILURE)
+        .expect(
+          "ゲームIDには数字・アルファベット小文字・ハイフンのみ使用できます。"
+        )
+        .end(done);
+    });
+    it("既に存在する場合、上書き設定していなければアップロードできない", (done) => {
+      GameModel.create({
+        creatorId: creatorId,
+        gameId: gameId,
+        createdBy: myId,
+        totalFileSize: 100,
+      })
+        .then(() =>
+          fs.copy(
+            unityGameWindowsPath,
+            path.join(
+              __dirname,
+              "../",
+              DIRECTORY_UPLOADS_DESTINATION,
+              creatorId,
+              gameId,
+              unityGameWindowsName
+            )
+          )
+        )
+        .then(() => {
+          request(app)
+            .post("/upload/unity")
+            .set("x-creator-id", creatorId)
+            .set("x-game-id", gameId)
+            .set("x-overwrites-existing", "false")
+            .attach(FIELD_WINDOWS, unityGameWindowsPath)
+            .expect(STATUS_CODE_FAILURE)
+            .expect(
+              "ゲームが既に存在しています。上書きする場合はチェックボックスにチェックを入れてください"
+            )
+            .end(done);
+        });
+    });
+    it("別人の投稿したゲームは上書きアップロードできない", (done) => {
+      GameModel.create({
+        creatorId: creatorId,
+        gameId: gameId,
+        createdBy: theirId,
+        totalFileSize: 100,
+      })
+        .then(() =>
+          fs.copy(
+            unityGameWindowsPath,
+            path.join(
+              __dirname,
+              "../",
+              DIRECTORY_UPLOADS_DESTINATION,
+              creatorId,
+              gameId,
+              unityGameWindowsName
+            )
+          )
+        )
+        .then(() => {
+          request(app)
+            .post("/upload/unity")
+            .set("x-creator-id", creatorId)
+            .set("x-game-id", gameId)
+            .set("x-overwrites-existing", "true")
+            .attach(FIELD_WINDOWS, unityGameWindowsPath)
+            .expect(STATUS_CODE_FAILURE)
+            .expect(
+              "別の人が既に投稿したゲームがあります。上書きすることはできません。"
+            )
+            .end(done);
+        });
+    });
     it("条件を満たしていればアップロードできる", (done) => {
       request(app)
         .post("/upload/unity")
@@ -34,7 +157,15 @@ describe("Unityゲームのアップロード", () => {
         .set("x-game-id", gameId)
         .set("x-overwrites-existing", "false")
         .attach(FIELD_WINDOWS, unityGameWindowsPath)
-        .expect(200)
+        .expect(STATUS_CODE_SUCCESS)
+        .expect(
+          `{"paths":["${path.join(
+            URL_PREFIX_GAME,
+            creatorId,
+            gameId,
+            FIELD_WINDOWS
+          )}"]}`
+        )
         .end(done);
     });
   });
