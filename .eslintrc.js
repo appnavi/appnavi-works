ee107f5c0c5ee773eac34715db8e7262394493f3
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-03-05 12:16:01 +0900

    eslintのアップデートによる設定変更を適用

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/@typescript-eslint',
+    'prettier'
   ],
   rules: {
     'sort-imports':0,
