b3d4dbf0cb00e663da18a02d3152e0ede396a3ba
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-04-27 00:34:16 +0900

    passport関連をTypescriptに移行

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,9 +1,11 @@
-const SlackStrategy = require("passport-slack").Strategy;
+import { PassportStatic } from "passport";
+import OAuth2Strategy from "passport-oauth2";
+import { Strategy as SlackStrategy } from "passport-slack";
 const strategy = new SlackStrategy(
   {
-    clientID: process.env["SLACK_CLIENT_ID"],
-    clientSecret: process.env["SLACK_CLIENT_SECRET"],
-    callbackURL: process.env["SLACK_REDIRECT_URI"],
+    clientID: process.env["SLACK_CLIENT_ID"] ?? "",
+    clientSecret: process.env["SLACK_CLIENT_SECRET"] ?? "",
+    callbackURL: process.env["SLACK_REDIRECT_URI"] ?? "",
     skipUserProfile: false,
     user_scope: [
       "identity.basic",
@@ -12,17 +14,23 @@ const strategy = new SlackStrategy(
       "identity.team",
     ], // default
   },
-  function (accessToken, refreshToken, profile, done) {
+  function (
+    accessToken: string,
+    refreshToken: string,
+    profile: Express.User,
+    done: OAuth2Strategy.VerifyCallback
+  ) {
     return done(null, profile);
   }
 );
-function preparePassport(passport) {
+
+function preparePassport(passport: PassportStatic): void {
   passport.serializeUser(function (user, done) {
     done(null, user);
   });
 
   passport.deserializeUser(function (user, done) {
-    done(null, user);
+    done(null, user as Express.User);
   });
 
   passport.use(strategy);
