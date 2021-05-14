78cca53205838f27b82d82d3ff8d443d8e4315cc
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-05-15 00:24:05 +0900

    Docker Composeの改善
    - MongoDBを包含
    - docker-compose.ymlの内容繰り返しを改善
    - MongoDBのportsをexposeに変更

--- production.sh
+++ production.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose -p game-upload -f docker-compose.yml up -d $@
+docker-compose -p game-upload -f docker-compose.yml -f docker-compose.yml up -d $@
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose -p game-upload-test -f docker-compose.test.yml up -d $@
+docker-compose -p game-upload-test -f docker-compose.yml -f docker-compose.test.yml up -d $@
