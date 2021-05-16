f51f08fd8570e378aef5010a90d626404527e237
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-05-16 20:12:34 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,27 @@ import { app } from "./app";
 import * as logger from "./modules/logger";
 import { getEnv, getEnvNumber } from "./utils/helpers";
 
-mongoose
-  .connect(getEnv("DATABASE_URL"), {
+async function prepareDatabase(): Promise<void> {
+  await mongoose.connect(getEnv("DATABASE_URL"), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  });
+  const db = mongoose.connection;
+  db.once("open", () => {
+    logger.system.info("データベースに接続しました。");
+  });
+  db.on("stop", (error) => {
+    logger.system.error("データベース関連のエラーが発生しました。", error);
+  });
+}
+
+prepareDatabase()
   .then(() => {
-    const db = mongoose.connection;
-    db.once("open", () => {
-      logger.system.info("データベースに接続しました。");
-    });
-    db.on("stop", (error) => {
-      logger.system.error("データベース関連のエラーが発生しました。", error);
-    });
     const port = getEnvNumber("PORT");
     app.listen(port, () => {
       logger.system.info(`Listening on port ${port}`);
     });
   })
   .catch((error) => {
-    logger.system.error("データベース関連のエラーが発生しました。", error);
+    logger.system.error("起動に失敗しました", error);
   });
