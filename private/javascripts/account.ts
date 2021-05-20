f734a37272d233a85703a0a72a81ec35d6c453a4
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-05-20 11:25:15 +0900

    リファクタリング

--- private/javascripts/account.ts
+++ private/javascripts/account.ts
@@ -32,50 +32,54 @@ document
     request.send(data);
   });
 
-document.querySelectorAll(".restoreBackupButton").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const creatorId = btn.attributes.getNamedItem("data-creator-id")!.value;
-    const workId = btn.attributes.getNamedItem("data-work-id")!.value;
-    const backupName = btn.attributes.getNamedItem("data-backup-name")!.value;
-    const message = document.createElement("p");
-    message.textContent = `バックアップ${backupName}を復元しますか？`;
-    showConfirmDialog(
-      "確認",
-      message,
-      {
-        label: "復元する",
-        onPresed: () => {
-          restoreBackup(creatorId, workId, backupName);
+document
+  .querySelectorAll<HTMLButtonElement>(".restoreBackupButton")
+  .forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const creatorId = btn.dataset["creatorId"];
+      const workId = btn.dataset["workId"];
+      const backupName = btn.dataset["backupName"];
+      const message = document.createElement("p");
+      message.textContent = `バックアップ${backupName}を復元しますか？`;
+      showConfirmDialog(
+        "確認",
+        message,
+        {
+          label: "復元する",
+          onPresed: () => {
+            restoreBackup(creatorId, workId, backupName);
+          },
         },
-      },
-      { label: "キャンセル" }
-    );
+        { label: "キャンセル" }
+      );
+    });
   });
-});
-document.querySelectorAll(".deleteBackupButton").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const creatorId = btn.attributes.getNamedItem("data-creator-id")!.value;
-    const workId = btn.attributes.getNamedItem("data-work-id")!.value;
-    const backupName = btn.attributes.getNamedItem("data-backup-name")!.value;
-    const message = document.createElement("p");
-    message.textContent = `バックアップ${backupName}を削除しますか？`;
-    showConfirmDialog(
-      "確認",
-      message,
-      {
-        label: "削除する",
-        classes: ["waves-effect", "waves-light", "btn", "red"],
-        onPresed: () => {
-          deleteBackup(creatorId, workId, backupName);
+document
+  .querySelectorAll<HTMLButtonElement>(".deleteBackupButton")
+  .forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const creatorId = btn.dataset["creatorId"];
+      const workId = btn.dataset["workId"];
+      const backupName = btn.dataset["backupName"];
+      const message = document.createElement("p");
+      message.textContent = `バックアップ${backupName}を削除しますか？`;
+      showConfirmDialog(
+        "確認",
+        message,
+        {
+          label: "削除する",
+          classes: ["waves-effect", "waves-light", "btn", "red"],
+          onPresed: () => {
+            deleteBackup(creatorId, workId, backupName);
+          },
         },
-      },
-      {
-        label: "キャンセル",
-        classes: ["waves-effect", "waves-light", "btn-flat"],
-      }
-    );
+        {
+          label: "キャンセル",
+          classes: ["waves-effect", "waves-light", "btn-flat"],
+        }
+      );
+    });
   });
-});
 
 function restoreBackup(creatorId: string, workId: string, backupName: string) {
   const request = new XMLHttpRequest();
