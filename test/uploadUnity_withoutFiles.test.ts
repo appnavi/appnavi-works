1814cc766cc4122eed547644a4432e271bdead38
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-05-21 22:16:57 +0900

    ファイルあり・なしで単体テストを分ける
    ファイルありのみjestでmulterをmock

--- test/uploadUnity_withoutFiles.test.ts
+++ test/uploadUnity_withoutFiles.test.ts
@@ -1,93 +1,10 @@
 import express from "express";
 import * as multer from "multer";
 import { Readable } from "stream";
-jest.mock("multer");
-
-// @ts-ignore
-multer.mockImplementation(() => {
-  return {
-    none() {
-      return (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        throw new Error("MOCK ERROR");
-      };
-    },
-    fields(fields: string) {
-      return (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        const createMockFile = (
-          fieldname: string,
-          foldername: string,
-          subfoldername: string,
-          filename: string,
-          mimetype: string,
-          size: number
-        ): Express.Multer.File => {
-          const destination = path.join(
-            DIRECTORY_UPLOADS_DESTINATION,
-            creatorId,
-            workId,
-            fieldname
-          );
-          return {
-            fieldname,
-            originalname: path.join(foldername, subfoldername, filename),
-            encoding: "7bit",
-            mimetype,
-            destination,
-            filename,
-            path: path.join(destination, subfoldername, filename),
-            size,
-            buffer: Buffer.from([]),
-            stream: Readable.from([]),
-          };
-        };
-        req.files = {
-          [FIELD_WEBGL]: [
-            createMockFile(
-              FIELD_WEBGL,
-              "unity-webgl",
-              "",
-              "index.html",
-              "text/html",
-              100
-            ),
-            createMockFile(
-              FIELD_WEBGL,
-              "unity-webgl",
-              "subfolder",
-              "script.js",
-              "text/javascript",
-              200
-            ),
-          ],
-          [FIELD_WINDOWS]: [
-            createMockFile(
-              FIELD_WINDOWS,
-              "",
-              "",
-              "unity-zip.zip",
-              "application/zip",
-              300
-            ),
-          ],
-        };
-        return next();
-      };
-    },
-  };
-});
-
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../src/app";
-import { fields, FIELD_WEBGL, FIELD_WINDOWS } from "../src/routes/upload";
+import { FIELD_WEBGL, FIELD_WINDOWS } from "../src/routes/upload";
 import {
   URL_PREFIX_WORK,
   DIRECTORY_UPLOADS_DESTINATION,
@@ -116,7 +33,7 @@ const workId = "work";
 const unityWorkWindowsName = "unity-correct-zip.zip";
 const unityWorkWindowsPath = path.join(__dirname, unityWorkWindowsName);
 
-describe("Unity作品のアップロード（ファイルあり）", () => {
+describe("Unity作品のアップロード（ファイルなし）", () => {
   beforeAll(async () => {
     await connectDatabase();
   });
@@ -208,19 +125,13 @@ describe("Unity作品のアップロード（ファイルあり）", () => {
           .end(done);
       });
     });
-    it("条件を満たしていればアップロードできる", (done) => {
+    it("作品がなければアップロードできない", (done) => {
       request(app)
         .post("/upload/unity")
         .set(HEADER_CREATOR_ID, creatorId)
         .set(HEADER_WORK_ID, workId)
-        .expect(STATUS_CODE_SUCCESS)
-        .expect(
-          JSON.stringify({
-            paths: fields.map((field) =>
-              path.join(URL_PREFIX_WORK, creatorId, workId, field.name)
-            ),
-          })
-        )
+        .expect(STATUS_CODE_BAD_REQUEST)
+        .expect(NO_FILES)
         .end(done);
     });
   });
