80aa74a249a543a8826df7d41f0febde370b1f59
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-05-29 23:51:55 +0900

    リファクタリング

--- test/httpPOST.test.ts
+++ test/httpPOST.test.ts
@@ -2,27 +2,21 @@ import request from "supertest";
 import { app } from "../src/app";
 import {
   STATUS_CODE_BAD_REQUEST,
-  STATUS_CODE_UNAUTHORIZED,
   ERROR_MESSAGE_CREATOR_ID_REQUIRED as CREATOR_ID_REQUIRED,
   ERROR_MESSAGE_CREATOR_ID_INVALID as CREATOR_ID_INVALID,
-  ERROR_MESSAGE_WORK_ID_REQUIRED as WORK_ID_REQUIRED,
-  ERROR_MESSAGE_WORK_ID_INVALID as WORK_ID_INVALID,
-  ERROR_MESSAGE_DIFFERENT_USER as DIFFERENT_USER,
-  ERROR_MESSAGE_STORAGE_FULL as STORAGE_FULL,
   ERROR_MESSAGE_NO_FILES as NO_FILES,
   HEADER_CREATOR_ID,
   HEADER_WORK_ID,
   STATUS_CODE_SUCCESS,
 } from "../src/utils/constants";
-import { getEnvNumber } from "../src/utils/helpers";
-import { login, logout, myId, theirId } from "./auth";
+import { login, logout, myId } from "./auth";
 import {
   clearData,
   connectDatabase,
   ensureUploadFoldersExist,
   INVALID_ID,
 } from "./common";
-import { UserModel, WorkModel } from "../src/models/database";
+import { UserModel } from "../src/models/database";
 
 const creatorId = "creator-3";
 const workId = "work-3";
