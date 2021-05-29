fae0a216c2abb0d84f6145b93bb2e7f0fac2139c
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-05-29 16:36:53 +0900

    ユーザー一覧と作品一覧ページ追加

--- src/routes/db.ts
+++ src/routes/db.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import { WorkModel, UserModel } from "../models/database";
 import { ensureAuthenticated } from "../services/auth";
+import { URL_PREFIX_WORK } from "../utils/constants";
 import { render } from "../utils/helpers";
 
 const dbRouter = express.Router();
 
 dbRouter.use(ensureAuthenticated);
-
+dbRouter.get("/works", function (req, res, next) {
+  WorkModel.find()
+    .then((works) => {
+      render("db/works", req, res, {
+        urlPrefix: URL_PREFIX_WORK,
+        works: works,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 dbRouter.get("/works/raw", function (req, res, next) {
   WorkModel.find()
     .then((works) => {
@@ -18,7 +30,17 @@ dbRouter.get("/works/raw", function (req, res, next) {
       next(err);
     });
 });
-
+dbRouter.get("/users", function (req, res, next) {
+  UserModel.find()
+    .then((users) => {
+      render("db/users", req, res, {
+        users: users,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 dbRouter.get("/users/raw", function (req, res, next) {
   UserModel.find()
     .then((users) => {
