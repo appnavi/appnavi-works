ebd5638433091892e49d850e7b20967805d0ff6c
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-06-11 23:50:52 +0900

    リネーム

--- production.sh
+++ production.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml -p appnavi-works up -d $@
+docker-compose -f docker-compose.yml -f docker-compose.production.yml -p appnavi-works up -d $@
