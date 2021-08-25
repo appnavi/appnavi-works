c3227763dc882bbeca2cc116f9a414793d02a543
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-08-25 14:00:31 +0900

    developmentはローカルで実行するように

--- serve-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -z `docker ps --format "{{.Names}}" | grep appnavi-works_development` ]; then
-  echo -e "\e[31m\`bash dev.sh up\`を実行した後にこのコマンドを実行してください。\e[m"
-  exit 1
-fi
-
-docker exec -it -u node appnavi-works_development yarn run dev
