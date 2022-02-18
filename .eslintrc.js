df650124c8bfab0daa4d7d595aeed2854e4b67e8
Juris710 <50994088+Juris710@users.noreply.github.com>
2022-02-18 14:18:54 +0900

    Eslintが通るように

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,34 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019, // Node.js 12の場合は2019、他のバージョンのNode.jsを利用している場合は場合は適宜変更する
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json']
-  },
-  plugins: [
-    '@typescript-eslint',
-    'import'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier'
-  ],
-  rules: {
-    'sort-imports':0,
-    'import/order': [
-      2,
-      {
-        'alphabetize': {
-          'order': 'asc'
-        }
-      }
-    ]
-  },
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2019, // Node.js 12の場合は2019、他のバージョンのNode.jsを利用している場合は場合は適宜変更する
+        project: ['./tsconfig.json']
+    },
+    plugins: [
+        '@typescript-eslint',
+        'import'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier'
+    ],
+    rules: {
+        'sort-imports': 0,
+        'import/order': [
+            2,
+            {
+                'alphabetize': {
+                    'order': 'asc'
+                }
+            }
+        ]
+    },
 };
