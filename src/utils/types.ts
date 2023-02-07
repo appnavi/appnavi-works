fc2e0f59741e1e98eab201f5c24940ab9a34798d
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-02-07 23:27:53 +0900

    不要なコード削除

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,16 +1,5 @@
 import { z } from "zod";
 
-export function isObject(x: unknown): x is Record<string, unknown> {
-  return typeof x === "object" && x != null;
-}
-
-export function isError(x: unknown): x is { name: string; message: string } {
-  return (
-    isObject(x) &&
-    typeof x["name"] === "string" &&
-    typeof x["message"] === "string"
-  );
-}
 const expressUserSchema = z.object({
   id: z.string(),
   name: z.string(),
