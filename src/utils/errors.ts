069d9a095bd32fbece469bb21a7713eed0ae7132
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-02-19 12:33:47 +0900

    リファクタリング

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -24,26 +24,17 @@ export class UnauthorizedError extends HttpError {
     logParams: unknown = undefined
   ) {
     super(STATUS_CODE_UNAUTHORIZED, logMessage, logParams);
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 export class NotFoundError extends HttpError {
   constructor() {
     super(STATUS_CODE_NOT_FOUND, "ページが見つかりませんでした");
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
 export class CsrfError extends HttpError {
   constructor(logMessage: string) {
     super(STATUS_CODE_FORBIDDEN, logMessage);
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
@@ -54,55 +45,32 @@ export class BadRequestError extends HttpError {
     logParams: unknown = undefined
   ) {
     super(STATUS_CODE_BAD_REQUEST, logMessage, logParams);
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
 export class UploadError extends BadRequestError {
   constructor(errors: unknown[] = [], logParams: unknown = undefined) {
     super("アップロードに失敗しました。", errors, logParams);
-
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
 export class RenameWorkError extends BadRequestError {
   constructor(errors: unknown[] = [], logParams: unknown = undefined) {
     super("作品のリネームに失敗しました。", errors, logParams);
-
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 export class DeleteWorkError extends BadRequestError {
   constructor(errors: unknown[] = [], logParams: unknown = undefined) {
     super("作品のリネームに失敗しました。", errors, logParams);
-
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 export class RestoreBackupError extends BadRequestError {
   constructor(errors: unknown[] = [], logParams: unknown = undefined) {
     super("バックアップの復元に失敗しました。", errors, logParams);
-
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 export class DeleteGuestUserError extends BadRequestError {
   constructor(errors: unknown[] = [], logParams: unknown = undefined) {
     super("ゲストユーザー削除に失敗しました。", errors, logParams);
-
-    this.name = new.target.name;
-    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
-    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
