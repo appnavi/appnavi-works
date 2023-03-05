616220a85619f99767edc1083cc79376409ede3f
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-05 23:27:15 +0900

    react-router-dom 導入

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="container flex-grow flex-shrink-0 basis-auto"></main>
+      <main className="container flex-grow flex-shrink-0 basis-auto">
+        <RouterProvider router={router} />
+      </main>
       <Footer />
     </>
   );
