df2a781eb2183aa69ed21b0cc7f5f65e7dd1d21b
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-05 22:03:06 +0900

    クライアントにprettier導入

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -9,7 +9,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
     ],
     "overrides": [
     ],
