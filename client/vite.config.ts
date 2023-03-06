c9d623c0b0016b8f8d2c8a6ba1287803a804cf2b
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-06 23:47:08 +0900

    バンドルのサイズを可視化できるように

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), visualizer()],
   server: {
     watch: {
       usePolling: true,
