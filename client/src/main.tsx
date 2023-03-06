2bf413a45144fba700ab24467efb342efdc90a8a
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-06 23:41:02 +0900

    ビルド後のサイズ削減

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import '@materializecss/materialize/dist/css/materialize.css';
-import '@materializecss/materialize/dist/js/materialize.min';
+// import '@materializecss/materialize/dist/js/materialize.min';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
