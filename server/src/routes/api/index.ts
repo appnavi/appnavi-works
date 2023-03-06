d9fe80d23a12f5fcd98b1d3861be9d45730e37b1
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-06 16:44:17 +0900

    trpcにcontext追加

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,5 +1,6 @@
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { Router } from "express";
+import { createContext } from "../../utils/trpc";
 import { accountRouter } from "./account";
 import { trpcRouter } from "./trpc";
 import { uploadRouter } from "./upload";
@@ -10,6 +11,6 @@ const apiRouter = Router();
 
 apiRouter.use("/account", accountRouter);
 apiRouter.use("/upload", uploadRouter);
-apiRouter.use(createExpressMiddleware({ router: trpcRouter }))
+apiRouter.use(createExpressMiddleware({ router: trpcRouter, createContext: createContext }))
 
 export { apiRouter };
--- server/src/utils/trpc.ts
+++ server/src/utils/trpc.ts
@@ -1,5 +1,24 @@
-import { initTRPC } from "@trpc/server";
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+import { CreateExpressContextOptions } from "@trpc/server/adapters/express";
+import { fromZodError } from "zod-validation-error";
+import { User } from "../common/types"
+import { system } from "../modules/logger";
 
-export const t = initTRPC.create();
+const OptionalUser = User.optional();
+
+export function createContext({ req }: CreateExpressContextOptions) {
+  const parsed = OptionalUser.safeParse(req.user)
+  if (!parsed.success) {
+    system.error("ユーザーが不正です", fromZodError(parsed.error))
+    return {
+      user: undefined
+    }
+  }
+  return {
+    user: parsed.data
+  }
+}
+
+export const t = initTRPC.context<inferAsyncReturnType<typeof createContext>>().create();
 
 export const publicProcedure = t.procedure;
