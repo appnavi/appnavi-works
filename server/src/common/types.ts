52e277b682e138df339949b9d6064f22ab16e1ca
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-07 11:01:17 +0900

    型修正

--- server/src/common/types.ts
+++ server/src/common/types.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 export const User = z.object({
   id: z.string(),
   name: z.string(),
-  avatar_url: z.string().url(),
+  avatar_url: z.string().url().optional(),
   type: z.enum(["Slack", "Guest"]),
 })
 export type User = z.infer<typeof User>
