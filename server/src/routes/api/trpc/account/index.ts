8bcbfd034e0456872e15b54b04c29c293aed9693
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-11 11:42:36 +0900

    リファクタリング

--- server/src/routes/api/trpc/account/index.ts
+++ server/src/routes/api/trpc/account/index.ts
@@ -2,18 +2,23 @@ import { TRPCError } from "@trpc/server";
 import { UserModel } from "../../../../models/database";
 import { t, authenticatedProcedure } from "../../../../utils/trpc";
 
+async function findUserById(userId: string) {
+  const users = await UserModel.find({ userId });
+  if (users.length > 1) {
+    throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+  }
+  if (users.length === 0) {
+    return null
+  }
+  return users[0];
+}
+
 
 export const accountRouter = t.router({
   getDefaultCreatorId: authenticatedProcedure.query(async ({ ctx }) => {
     const userId = ctx.user.id;
-    const users = await UserModel.find({ userId });
-    if (users.length > 1) {
-      throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
-    }
-    if (users.length == 0) {
-      return null;
-    }
-    return users[0].defaultCreatorId ?? null
+    const user = await findUserById(userId);
+    return user?.defaultCreatorId ?? null
   }),
 });
 
