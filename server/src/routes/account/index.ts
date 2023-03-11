64ba1b29234c5028058bd12d3947e2a192f3d233
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-03-11 17:38:26 +0900

    不要コード削除

--- server/src/routes/account/index.ts
+++ server/src/routes/account/index.ts
@@ -1,36 +1,13 @@
 import express from "express";
-import { WorkModel } from "../../models/database";
 import {
   ensureAuthenticated,
-  findOrCreateUser,
-  getDefaultCreatorId,
-  getUserIdOrThrow,
 } from "../../services/auth";
-import { URL_PREFIX_WORK } from "../../utils/constants";
-import { render, wrap } from "../../utils/helpers";
 import { guestRouter } from "./guest";
 
 const accountRouter = express.Router();
 
 accountRouter.use(ensureAuthenticated);
 
-accountRouter.get(
-  "/",
-  wrap(async (req, res) => {
-    const userId = getUserIdOrThrow(req);
-    const defaultCreatorId = await getDefaultCreatorId(req);
-    const works = await WorkModel.find({
-      owner: userId,
-    });
-    const user = await findOrCreateUser(userId);
-    render("account", req, res, {
-      defaultCreatorId,
-      works,
-      urlPrefix: URL_PREFIX_WORK,
-      creatorIds: user.creatorIds,
-    });
-  })
-);
 accountRouter.use("/guest", guestRouter);
 
 export { accountRouter };
