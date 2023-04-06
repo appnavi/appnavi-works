9705d6cf94a8d05622e15b1bfe48df2b97d894ac
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-04-06 22:05:06 +0900

    作品のリネーム時にユーザーのcreatorIdsが更新されない問題を修正

--- server/src/routes/api/trpc/account/work/index.ts
+++ server/src/routes/api/trpc/account/work/index.ts
@@ -157,8 +157,7 @@ async function renameWork(
       );
     }
   }
-
-  if (work.creatorId !== renamedCreatorId) {
+  if (creatorId !== renamedCreatorId) {
     await updateCreatorIds(userId, renamedCreatorId);
   }
   await work.save();
