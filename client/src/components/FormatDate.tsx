ece669a3d8ecc68478325235fd991e45ff0c0f66
Juris710 <50994088+Juris710@users.noreply.github.com>
2023-08-15 21:42:07 +0900

    Fix Date month

--- client/src/components/FormatDate.tsx
+++ client/src/components/FormatDate.tsx
@@ -1,7 +1,7 @@
 export const FormatDate = ({ date }: { date: Date }) => {
   return (
     <>
-      {date.getFullYear()}年{date.getMonth()}月{date.getDate()}日
+      {date.getFullYear()}年{date.getMonth() + 1}月{date.getDate()}日
       {date.getHours()}時{date.getMinutes()}分{date.getSeconds()}秒
     </>
   );
